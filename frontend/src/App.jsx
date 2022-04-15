e39b19faf4cd04190ac2f9ee547975638b7bac17
Manjeet44 <andrew_vab@hotmail.com>
2022-04-15 20:34:01 +0200

    cabeza

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,29 @@ import Registrar from './paginas/Registrar';
 import NuevoPassword from './paginas/NuevoPassword';
 import OlvidePassword from './paginas/OlvidePassword';
 import ConfirmarCuenta from './paginas/ConfirmarCuenta';
+import RutaProtegida from './layouts/RutaProtegida';
+import Proyectos from './paginas/Proyectos';
 
+import {AuthProvaider} from './context/AuthProvaider';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout/>}>
-          <Route index element={<Login/>} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-
-        </Route>
-      </Routes>
+      <AuthProvaider>
+        <Routes>
+          <Route path="/" element={<AuthLayout/>}>
+            <Route index element={<Login/>} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+          </Route>
+          <Route path="/proyectos" element={<RutaProtegida/>}>
+            <Route index element={<Proyectos/>} />
+          </Route>
+        </Routes>
+      </AuthProvaider>
     </BrowserRouter>
   )
 }
